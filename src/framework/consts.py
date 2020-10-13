52a99f7c6edfb985f70246b7b72f0edc44303a5a
Alexander Sidorov <alexander@sidorov.dev>
2020-10-13 22:13:19 +0300

    fixed settings, refactored consts, moved static to repo

--- src/framework/consts.py
+++ src/framework/consts.py
@@ -15,4 +15,7 @@ _this_file_path = Path(__file__).resolve()
 DIR_FRAMEWORK = _this_file_path.parent.resolve()
 
 DIR_SRC = DIR_FRAMEWORK.parent.resolve()
-DIR_STATIC = (DIR_SRC / "static").resolve()
+
+DIR_REPO = DIR_SRC.parent.resolve()
+
+DIR_STATIC = (DIR_REPO / "static").resolve()
