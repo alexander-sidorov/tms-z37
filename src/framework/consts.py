4da0ea4823dad4417bbdea1dd595c5373665e225
Alexander Sidorov <alexander@sidorov.dev>
2020-10-13 22:09:00 +0300

    decomposed html

--- src/framework/consts.py
+++ src/framework/consts.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 SERVER_RUNNING_BANNER = """
 +----------------------------------------+
 |             SERVER WORKS!              |
@@ -7,3 +9,10 @@ Visit http://{host}:{port}
 
 ..........................................
 """
+
+_this_file_path = Path(__file__).resolve()
+
+DIR_FRAMEWORK = _this_file_path.parent.resolve()
+
+DIR_SRC = DIR_FRAMEWORK.parent.resolve()
+DIR_STATIC = (DIR_SRC / "static").resolve()
