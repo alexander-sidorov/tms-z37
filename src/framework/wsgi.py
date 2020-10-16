c50722523fd78f4a8f63a8185903c901258b453b
Alexander Sidorov <alexander@sidorov.dev>
2020-10-16 21:42:35 +0300

    fdshfbdshjfbds

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -2,20 +2,41 @@ from framework.consts import DIR_STATIC
 
 
 def application(environ, start_response):
-    status = "200 OK"
-    headers = {
-        "Content-type": "text/html",
-    }
+    url = environ["PATH_INFO"]
+    if url == "/xxx/":
+        status = "200 OK"
+        headers = {
+            "Content-type": "text/css",
+        }
+        payload = read_from_styles_css()
+        start_response(status, list(headers.items()))
 
-    payload = read_from_index_html()
+        yield payload
+    else:
+        status = "200 OK"
+        headers = {
+            "Content-type": "text/html",
+        }
 
-    start_response(status, list(headers.items()))
+        payload = read_from_index_html()
 
-    yield payload
+        start_response(status, list(headers.items()))
+
+        yield payload
 
 
 def read_from_index_html():
     path = DIR_STATIC / "index.html"
+
+    with path.open("r") as fp:
+        payload = fp.read()
+
+    payload = payload.encode()
+    return payload
+
+
+def read_from_styles_css():
+    path = DIR_STATIC / "styles.css"
 
     with path.open("r") as fp:
         payload = fp.read()
