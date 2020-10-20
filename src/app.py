187d73045e80be1e6631304f572468c8e31bbf70
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-10-20 19:38:35 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from wsgiref.simple_server import make_server
 
 from framework import settings
 from framework.consts import SERVER_RUNNING_BANNER
-from framework.wsgi import application
+from framework.wsgi import application as app
 
 
 def run():
