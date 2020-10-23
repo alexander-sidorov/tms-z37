8b26da4299e5302f10e0e510d0eca421ae9593e4
Alexander Sidorov <alexander@sidorov.dev>
2020-10-23 21:54:35 +0300

    xxx

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -1,3 +1,4 @@
+import random
 from mimetypes import guess_type
 
 from framework.consts import DIR_STATIC
@@ -6,18 +7,23 @@ from framework.consts import DIR_STATIC
 def application(environ, start_response):
     url = environ["PATH_INFO"]
 
-    file_names = {
-        "/xxx/": "styles.css",
-        "/logo.png/": "logo.png",
-    }
+    file_names = {"/xxx/": "styles.css", "/logo.png/": "logo.png", "/": "index.html"}
 
-    file_name = file_names.get(url, "index.html")
+    file_name = file_names.get(url)
 
-    status = "200 OK"
-    headers = {
-        "Content-type": guess_type(file_name)[0],
-    }
-    payload = read_static(file_name)
+    if file_name is not None:
+        status = "200 OK"
+        headers = {
+            "Content-type": guess_type(file_name)[0],
+        }
+        payload = read_static(file_name)
+    else:
+        status = "404 Not Found"
+        headers = {
+            "Content-type": "text/plain",
+        }
+        payload = generate_404(environ)
+
     start_response(status, list(headers.items()))
 
     yield payload
@@ -30,3 +36,12 @@ def read_static(file_name: str) -> bytes:
         payload = fp.read()
 
     return payload
+
+
+def generate_404(environ) -> bytes:
+    url = environ["PATH_INFO"]
+    pin = random.randint(1, 1000)
+
+    msg = f"Hello world! Your path: {url} not found. Pin: {pin}"
+
+    return msg.encode()
