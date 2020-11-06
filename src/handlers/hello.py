c7e9087446cb43e2d2ae1bd211151f48bc4b498e
Alexander Sidorov <alexander@sidorov.dev>
2020-11-06 21:07:43 +0300

    introduced form

--- src/handlers/hello.py
+++ src/handlers/hello.py
@@ -5,14 +5,17 @@ from framework.utils import read_static
 
 
 def handle_hello(request: RequestT) -> ResponseT:
-    name = (request.query.get("name") or ["anon"])[0]
+    name = (request.query.get("name") or [None])[0]
 
     base = read_static("_base.html")
 
     base_html = base.content.decode()
     hello_html = read_static("hello.html").content.decode()
 
-    document = hello_html.format(name=name)
+    document = hello_html.format(
+        name_header=name or "anon",
+        name_value=name or "",
+    )
     document = base_html.format(xxx=document)
 
     resp = ResponseT(
