bd5fab3b5c119013ca01a497d13b540d14394ce4
Alexander Sidorov <alexander@sidorov.dev>
2020-11-06 20:53:40 +0300

    extracted html from hello handler

--- src/handlers/hello.py
+++ src/handlers/hello.py
@@ -8,12 +8,17 @@ def handle_hello(request: RequestT) -> ResponseT:
     name = (request.query.get("name") or ["anon"])[0]
 
     base = read_static("_base.html")
+
     base_html = base.content.decode()
-    document = base_html.format(xxx=f"<h2>hello {name}</h2>")
+    hello_html = read_static("hello.html").content.decode()
+
+    document = hello_html.format(name=name)
+    document = base_html.format(xxx=document)
 
     resp = ResponseT(
         status=build_status(200),
         headers={"Content-Type": base.content_type},
         payload=document.encode(),
     )
+
     return resp
