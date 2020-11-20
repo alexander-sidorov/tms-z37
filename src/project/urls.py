b321137bf81f51d8bd501b05eb3c4c00bb766b89
Alexander Sidorov <alexander@sidorov.dev>
2020-11-20 20:11:24 +0300

    added view for index page

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from django.http import HttpRequest
+from django.http import HttpResponse
 from django.urls import path
+
+
+def index(_request: HttpRequest) -> HttpResponse:
+    return HttpResponse("xxx")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index),
 ]
