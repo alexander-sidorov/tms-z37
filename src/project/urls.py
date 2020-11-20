0c7fd0b99278fbdf96cb43fb2a4ef9b5abfefa27
Alexander Sidorov <alexander@sidorov.dev>
2020-11-20 20:20:54 +0300

    implemented index view as previous

--- src/project/urls.py
+++ src/project/urls.py
@@ -3,12 +3,21 @@ from django.http import HttpRequest
 from django.http import HttpResponse
 from django.urls import path
 
+from framework.utils import read_static
+
 
 def index(_request: HttpRequest) -> HttpResponse:
-    return HttpResponse("xxx")
+    base = read_static("_base.html")
+    base_html = base.content.decode()
+    index_html = read_static("index.html").content.decode()
+
+    result = base_html.format(body=index_html)
+    result = result.encode()
+
+    return HttpResponse(result)
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", index),
+    path("admin/", admin.site.urls),
 ]
