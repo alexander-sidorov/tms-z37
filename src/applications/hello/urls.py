f3d0a8d490c5060577bb2852bc91577cc3994024
Alexander Sidorov <alexander@sidorov.dev>
2020-11-24 21:09:28 +0300

    splitted views

--- src/applications/hello/urls.py
+++ src/applications/hello/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from applications.hello.views import view_hello
+from applications.hello import views
 
 urlpatterns = [
-    path("", view_hello),
+    path("", views.view_hello_index),
+    path("greet/", views.view_hello_greet),
+    path("reset/", views.view_hello_reset),
 ]
