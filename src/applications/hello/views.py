cf23bbb63c3eb09705a510a197f47528a3f95ab2
Alexander Sidorov <alexander@sidorov.dev>
2020-11-24 21:47:51 +0300

    implemented greet and reset views

--- src/applications/hello/views.py
+++ src/applications/hello/views.py
@@ -1,16 +1,33 @@
 from django.http import HttpRequest
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.shortcuts import render
 
 
 def view_hello_index(request: HttpRequest) -> HttpResponse:
-    response = render(request, "hello/index.html")
+    name = request.session.get("name")
+    address = request.session.get("address")
+
+    context = {
+        "address_header": address or "nowhere",
+        "address_value": address or "",
+        "name_header": name or "anonymous",
+        "name_value": name or "",
+    }
+    response = render(request, "hello/index.html", context=context)
     return response
 
 
 def view_hello_greet(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("xxx")
+    name = request.POST.get("name")
+    address = request.POST.get("address")
+
+    request.session["name"] = name
+    request.session["address"] = address
+
+    return redirect("/h/")
 
 
 def view_hello_reset(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("xxx")
+    request.session.clear()
+    return redirect("/h/")
