cdd79a29152d1e911fcaa05d9569b3a2c20b6afe
Alexander Sidorov <alexander@sidorov.dev>
2020-12-01 19:31:20 +0300

    initialized app for blog

--- src/project/urls.py
+++ src/project/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 urlpatterns = [
     path("", include("applications.landing.urls")),
     path("admin/", admin.site.urls),
+    path("b/", include("applications.blog.urls")),
     path("h/", include("applications.hello.urls")),
 ]
