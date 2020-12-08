193f234b133e239bf32d3d0ac06c4a9785061674
Alexander Sidorov <alexander@sidorov.dev>
2020-12-08 22:55:39 +0300

    short names

--- src/applications/blog/urls.py
+++ src/applications/blog/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("new/", views.NewPostView.as_view(), name="new"),
     path("wipe/", views.WipeAllPostsView.as_view(), name="wipe"),
     path("post/<int:pk>/", views.PostView.as_view(), name="post"),
-    path("post/<int:pk>/delete/", views.DeletePostView.as_view(), name="delete-post"),
+    path("post/<int:pk>/delete/", views.DeletePostView.as_view(), name="delete"),
 ]
