111b5c40405c98b9241f4c54f94c0cd9a8292b73
Alexander Sidorov <alexander@sidorov.dev>
2020-12-08 20:09:45 +0300

    detail views

--- src/applications/blog/urls.py
+++ src/applications/blog/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 
 from applications.blog import views
 
+# app_name = "blog"
+
 urlpatterns = [
-    path("", views.AllPostsView.as_view()),
+    path("", views.AllPostsView.as_view(), name="all"),
     path("new/", views.NewPostView.as_view()),
     path("wipe/", views.WipeView.as_view()),
+    path("post/<int:pk>/", views.SinglePostView.as_view()),
+    # path("post/<int:pk>/update/", views.UpdatePostView.as_view()),
+    path("post/<int:pk>/delete/", views.DeletePostView.as_view()),
 ]
--- src/applications/blog/views.py
+++ src/applications/blog/views.py
@@ -1,9 +1,13 @@
 from typing import Dict
 
 from django import forms
+from django.urls import reverse
 from django.views.generic import CreateView
+from django.views.generic import DeleteView
+from django.views.generic import DetailView
 from django.views.generic import ListView
 from django.views.generic import RedirectView
+from django.views.generic import UpdateView
 
 from applications.blog.models import Post
 from framework.mixins import ExtendedContextMixin
@@ -37,3 +41,18 @@ class WipeView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         Post.objects.all().delete()
         return "/b/"
+
+
+class SinglePostView(DetailView):
+    template_name = "blog/post.html"
+    model = Post
+
+
+class UpdatePostView(UpdateView):
+    pass
+
+
+class DeletePostView(DeleteView):
+    http_method_names = ["post"]
+    model = Post
+    success_url = reverse("all")
